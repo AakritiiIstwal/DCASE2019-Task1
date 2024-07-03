737843332296990a5f61067f2dd4189e84887664
KI: Student (30.09.2024) <aistwal@dws-student-01.informatik.uni-mannheim.de>
2024-07-03 12:34:55 +0200

    python script for Training

--- DCASE_training_functions.py
+++ DCASE_training_functions.py
@@ -1,45 +1,89 @@
 import keras
 from keras import backend as K
+
+# from tensorflow import keras
+import tensorflow as tf
 import numpy as np
 import threading
 
-#for implementing warm restarts in learning rate
+
+# for implementing warm restarts in learning rate
 class LR_WarmRestart(keras.callbacks.Callback):
-    
-    def __init__(self,nbatch,initial_lr,min_lr,epochs_restart,Tmult):
+
+    def __init__(self, nbatch, initial_lr, min_lr, epochs_restart, Tmult):
         self.initial_lr = initial_lr
         self.min_lr = min_lr
         self.epochs_restart = epochs_restart
         self.nbatch = nbatch
-        self.currentEP=0
-        self.startEP=0
-        self.Tmult=Tmult
-        
+        self.currentEP = 0
+        self.startEP = 0
+        self.Tmult = Tmult
+
     def on_epoch_begin(self, epoch, logs={}):
-        if epoch+1<self.epochs_restart[0]:
+        if epoch + 1 < self.epochs_restart[0]:
             self.currentEP = epoch
         else:
-            self.currentEP = epoch+1
-            
-        if np.isin(self.currentEP,self.epochs_restart):
-            self.startEP=self.currentEP
-            self.Tmult=2*self.Tmult
-        
-    def on_epoch_end(self, epochs, logs={}):
-        lr = K.get_value(self.model.optimizer.lr)
-        print ('\nLearningRate:{:.6f}'.format(lr))
-    
-    def on_batch_begin(self, batch, logs={}):
-        pts = self.currentEP + batch/self.nbatch - self.startEP
-        decay = 1+np.cos(pts/self.Tmult*np.pi)
-        lr = self.min_lr+0.5*(self.initial_lr-self.min_lr)*decay
-        K.set_value(self.model.optimizer.lr,lr)
+            self.currentEP = epoch + 1
 
-        
+        if np.isin(self.currentEP, self.epochs_restart):
+            self.startEP = self.currentEP
+            self.Tmult = 2 * self.Tmult
+
+    def on_epoch_end(self, epochs, logs={}):
+        lr = self.model.optimizer.learning_rate.numpy()
+        print("\nLearningRate:{:.6f}".format(lr))
+
+    def on_batch_begin(self, batch, logs={}):
+        pts = self.currentEP + batch / self.nbatch - self.startEP
+        decay = 1 + np.cos(pts / self.Tmult * np.pi)
+        lr = self.min_lr + 0.5 * (self.initial_lr - self.min_lr) * decay
+        # K.set_value(self.model.optimizer.lr,lr)
+        self.model.optimizer.learning_rate.assign(lr)
+
+
+class LR_WarmRestartNew(keras.callbacks.Callback):
+    def __init__(self, nbatch, initial_lr, min_lr, epochs_restart, Tmult):
+        super(LR_WarmRestart, self).__init__()
+        self.initial_lr = initial_lr
+        self.min_lr = min_lr
+        self.epochs_restart = epochs_restart
+        self.nbatch = nbatch
+        self.currentEP = 0
+        self.startEP = 0
+        self.Tmult = Tmult
+
+    def on_epoch_begin(self, epoch, logs=None):
+        if logs is None:
+            logs = {}
+        # Adjust the current epoch number and Tmult
+        self.currentEP = epoch
+        if np.isin(epoch, self.epochs_restart):
+            self.startEP = epoch
+            self.Tmult *= 2
+
+    def on_epoch_end(self, epoch, logs=None):
+        if logs is None:
+            logs = {}
+        # Log the current learning rate
+        lr = self.model.optimizer.learning_rate.numpy()
+        print(f"\nEpoch {epoch}: LearningRate: {lr:.6f}")
+
+    def on_batch_begin(self, batch, logs=None):
+        if logs is None:
+            logs = {}
+        # Calculate the new learning rate
+        pts = self.currentEP + batch / self.nbatch - self.startEP
+        decay = 1 + np.cos(pts / self.Tmult * np.pi)
+        lr = self.min_lr + 0.5 * (self.initial_lr - self.min_lr) * decay
+        # Set the learning rate
+        self.model.optimizer.learning_rate.assign(lr)
+
+
 class threadsafe_iter:
     """Takes an iterator/generator and makes it thread-safe by
     serializing call to the `next` method of given iterator/generator.
     """
+
     def __init__(self, it):
         self.it = it
         self.lock = threading.Lock()
@@ -50,19 +94,21 @@ class threadsafe_iter:
     def __next__(self):
         with self.lock:
             return self.it.__next__()
-        
-        
+
+
 def threadsafe_generator(f):
-    """A decorator that takes a generator function and makes it thread-safe.
-    """
+    """A decorator that takes a generator function and makes it thread-safe."""
+
     def g(*a, **kw):
         return threadsafe_iter(f(*a, **kw))
+
     return g
 
 
-
-class MixupGenerator():
-    def __init__(self, X_train, y_train, batch_size=32, alpha=0.2, shuffle=True, crop_length=400): #datagen=None):
+class MixupGenerator:
+    def __init__(
+        self, X_train, y_train, batch_size=32, alpha=0.2, shuffle=True, crop_length=400
+    ):  # datagen=None):
         self.X_train = X_train
         self.y_train = y_train
         self.batch_size = batch_size
@@ -71,23 +117,34 @@ class MixupGenerator():
         self.sample_num = len(X_train)
         self.lock = threading.Lock()
         self.NewLength = crop_length
-        self.swap_inds = [1,0,3,2,5,4]
-        
+        self.swap_inds = [1, 0, 3, 2, 5, 4]
+
     def __iter__(self):
         return self
-    
-    @threadsafe_generator
+
+    # @threadsafe_generator
     def __call__(self):
-        with self.lock:
-            while True:
-                indexes = self.__get_exploration_order()
-                itr_num = int(len(indexes) // (self.batch_size * 2))
+        # with self.lock:
+        #     while True:
+        #         indexes = self.__get_exploration_order()
+        #         itr_num = int(len(indexes) // (self.batch_size * 2))
 
-                for i in range(itr_num):
-                    batch_ids = indexes[i * self.batch_size * 2:(i + 1) * self.batch_size * 2]
-                    X, y = self.__data_generation(batch_ids)
+        #         for i in range(itr_num):
+        #             batch_ids = indexes[i * self.batch_size * 2:(i + 1) * self.batch_size * 2]
+        #             X, y = self.__data_generation(batch_ids)
 
-                    yield X, y
+        #             yield X, y
+        while True:
+            indexes = self.__get_exploration_order()
+            itr_num = int(len(indexes) // (self.batch_size * 2))
+
+            for i in range(itr_num):
+                batch_ids = indexes[
+                    i * self.batch_size * 2 : (i + 1) * self.batch_size * 2
+                ]
+                X, y = self.__data_generation(batch_ids)
+
+                yield X, y
 
     def __get_exploration_order(self):
         indexes = np.arange(self.sample_num)
@@ -103,39 +160,42 @@ class MixupGenerator():
         X_l = l.reshape(self.batch_size, 1, 1, 1)
         y_l = l.reshape(self.batch_size, 1)
 
-        X1 = self.X_train[batch_ids[:self.batch_size]]
-        X2 = self.X_train[batch_ids[self.batch_size:]]
-        
-        for j in range(X1.shape[0]):
-            StartLoc1 = np.random.randint(0,X1.shape[2]-self.NewLength)
-            StartLoc2 = np.random.randint(0,X2.shape[2]-self.NewLength)
+        X1 = self.X_train[batch_ids[: self.batch_size]]
+        X2 = self.X_train[batch_ids[self.batch_size :]]
 
-            X1[j,:,0:self.NewLength,:] = X1[j,:,StartLoc1:StartLoc1+self.NewLength,:]
-            X2[j,:,0:self.NewLength,:] = X2[j,:,StartLoc2:StartLoc2+self.NewLength,:]
-            
-            if X1.shape[-1]==6:
-                #randomly swap left and right channels 
+        for j in range(X1.shape[0]):
+            StartLoc1 = np.random.randint(0, X1.shape[2] - self.NewLength)
+            StartLoc2 = np.random.randint(0, X2.shape[2] - self.NewLength)
+
+            X1[j, :, 0 : self.NewLength, :] = X1[
+                j, :, StartLoc1 : StartLoc1 + self.NewLength, :
+            ]
+            X2[j, :, 0 : self.NewLength, :] = X2[
+                j, :, StartLoc2 : StartLoc2 + self.NewLength, :
+            ]
+
+            if X1.shape[-1] == 6:
+                # randomly swap left and right channels
                 if np.random.randint(2) == 1:
-                    X1[j,:,:,:] = X1[j:j+1,:,:,self.swap_inds]
+                    X1[j, :, :, :] = X1[j : j + 1, :, :, self.swap_inds]
                 if np.random.randint(2) == 1:
-                    X2[j,:,:,:] = X2[j:j+1,:,:,self.swap_inds]
-            
-            
-        X1 = X1[:,:,0:self.NewLength,:]
-        X2 = X2[:,:,0:self.NewLength,:]
-        
+                    X2[j, :, :, :] = X2[j : j + 1, :, :, self.swap_inds]
+
+        X1 = X1[:, :, 0 : self.NewLength, :]
+        X2 = X2[:, :, 0 : self.NewLength, :]
+
         X = X1 * X_l + X2 * (1.0 - X_l)
 
         if isinstance(self.y_train, list):
             y = []
 
             for y_train_ in self.y_train:
-                y1 = y_train_[batch_ids[:self.batch_size]]
-                y2 = y_train_[batch_ids[self.batch_size:]]
+                y1 = y_train_[batch_ids[: self.batch_size]]
+                y2 = y_train_[batch_ids[self.batch_size :]]
                 y.append(y1 * y_l + y2 * (1.0 - y_l))
         else:
-            y1 = self.y_train[batch_ids[:self.batch_size]]
-            y2 = self.y_train[batch_ids[self.batch_size:]]
+            y1 = self.y_train[batch_ids[: self.batch_size]]
+            y2 = self.y_train[batch_ids[self.batch_size :]]
             y = y1 * y_l + y2 * (1.0 - y_l)
 
         return X, y
